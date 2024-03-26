5b2587fee0a1f52d85f65a92a8a441652819d810
AOSXAP <aosxap@gmail.com>
2024-03-26 19:38:04 +0200

    scrollable widgets

--- lib/main.dart
+++ lib/main.dart
@@ -27,10 +27,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-  // This class is the configuration for the state.
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -63,44 +59,28 @@ class _MyHomePageState extends State<MyHomePage> {
 
       Map<String,dynamic> comm =  Map<String,dynamic>();
 
-      if(jsonString != "") {
+      if(jsonString.isNotEmpty) {
         comm  = jsonDecode(jsonString) as Map<String, dynamic>;
       }
 
       setState(() {
         changeTitle = file_name;
-        if(js != "")_counter = comm[file_name]['DESCRIPTION'].toString(); 
+        print(file_name);
+        if(js.isNotEmpty){
+          _counter = comm[file_name]['DESCRIPTION'].toString(); 
+        }
       });
     }
 
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(changeTitle),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
