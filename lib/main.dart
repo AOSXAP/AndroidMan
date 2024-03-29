e67691741eff7ab652a9ffb9aa5c06ac8b73b2cc
AOSXAP <aosxap@gmail.com>
2024-03-29 13:33:39 +0200

    changed interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 import 'dart:io' as io;
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/widgets.dart';
@@ -36,7 +37,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   static String changeTitle = "Command";
   static final List _friendsList = [''];
+  static final List _all_comands = [[]];
   static final List commands_desc = [''];
+  static int index = 0;
 
   void _incrementCounter() {
     setState((){
@@ -70,9 +73,19 @@ class _MyHomePageState extends State<MyHomePage> {
         changeTitle = file_name;
         _friendsList.clear();
         commands_desc.clear();
+        _all_comands.clear();
+        index = 0;
         if(js.isNotEmpty){
           for(var key in comm[file_name].keys){
             _friendsList.add(comm[file_name][key].toString());
+            _all_comands.add([]);
+            //var paragraphs =  Map<String,dynamic>();
+            for(var para in comm[file_name][key]){
+              //print(para.toString());
+              _all_comands[index].add(para.toString());
+              //_friendsList.add(para.toString());
+            }
+            index += 1;
             commands_desc.add(key.toString());
           }
         }
@@ -83,13 +96,30 @@ class _MyHomePageState extends State<MyHomePage> {
 
 List<Widget> _getFriends(){
   List<Widget> friendsTextFields = [];
+  List<Widget> textFields = [];
+
+
   for(int i=0; i<_friendsList.length; i++){
+    textFields = [];
+    for(int j = 0; j < _all_comands[i].length; j++){
+        textFields.add(              
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 8),
+            child: Container(
+               child: Text(
+                _all_comands[i][j],
+                style: const TextStyle(
+                  color: Colors.white,
+
+                )))
+            )
+        );
+    }
   friendsTextFields.add(
     Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
       child: Container(
-        height: 200,
-        color: Color.fromARGB(255, 56, 56, 56),
+        //color: Color.fromARGB(255, 56, 56, 56),f
         alignment: Alignment.topLeft,
         child: SingleChildScrollView(
             child: Column(
@@ -101,15 +131,10 @@ List<Widget> _getFriends(){
                 style: const TextStyle(
                   fontSize: 20,
                   color: Color.fromARGB(255, 255, 255, 255),
-                  fontWeight: FontWeight.bold,
+                  fontWeight: FontWeight.bold
                 ),
               ),
-              Text(
-                _friendsList[i],
-                style: const TextStyle(
-                  color: Colors.white,
-                )
-              )
+              ...textFields
             ],
           ),
         )
@@ -129,7 +154,7 @@ return friendsTextFields;
           style: const TextStyle(
             fontSize: 30,
             color: Colors.white,
-            fontWeight: FontWeight.bold,
+            fontWeight: FontWeight.bold
           )
           ),
         backgroundColor: Color.fromARGB(255, 42, 42, 42),
