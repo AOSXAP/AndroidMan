f2b138032655ce1e49f0cb34c851cb05b08bf2d9
AOSXAP <aosxap@gmail.com>
2024-04-02 13:11:09 +0300

    made app work

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/readCommands.dart';
 import 'command.dart';
 
 class StatefulMenu extends StatefulWidget{
@@ -7,18 +8,43 @@ class StatefulMenu extends StatefulWidget{
 }
 
 class Menu extends State<StatefulMenu>{
-  List<Widget> listMMM=[];
+  List<Widget> menuEntries=[];
+
+  @override
+  Menu(){
+    _initListOfCommands();
+  }
+
+  void _initListOfCommands() async{
+    List commands = await readCommand();
+
+    setState(() {
+      for(var command in commands){
+        menuEntries.add(TextButton(
+            child: Text(command),
+          onPressed: (){
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => Command(command),
+              ),
+            );
+          },
+        ));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context){
     return SingleChildScrollView(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children:[
-          ...listMMM,
+          ...menuEntries,
           TextButton(
             onPressed: () {
               setState(() {
-                listMMM.add(const Text("muie"));
+                menuEntries.add(const Text("muie"));
               });
                 
             }, 
