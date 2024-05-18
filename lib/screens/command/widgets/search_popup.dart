cc8389b31a0abd51a041fedfef6eddead3690678
boroicamarius01@gmail.com <boroicamarius01@gmail.com>
2024-05-18 23:56:04 +0300

    optimized AlertDialog, rendering was slow fixed

--- lib/screens/command/widgets/search_popup.dart
+++ lib/screens/command/widgets/search_popup.dart
@@ -1,35 +1,34 @@
 import 'package:flutter/material.dart';
+
 import 'popup.dart';
 
-Widget popupSearchWidget(BuildContext context, Map<GlobalKey,String> searchUtility) {
+Widget popupSearchWidget(BuildContext context, Map<GlobalKey, String> searchUtility) {
   /// Description: returns a search widget that on submit launches a popup
 
   return FractionallySizedBox(
       widthFactor: 1, // between 0 and 1
       child: SizedBox(
           child: TextField(
-            onSubmitted: (String searchedTerm) {
-              /// List of paragraphs
-              List<String> collectedText = [];
+        onSubmitted: (String searchedTerm) {
+          /// List of paragraphs
+          List<String> collectedText = [];
 
-              /// foreach paragraph
-              for (MapEntry<GlobalKey, String> item in searchUtility.entries) {
-                /// if paragraph contains the searchedTerm, store it
-                if (item.value.contains(searchedTerm)) {
-                  collectedText.add(item.value);
-                }
-              }
+          /// foreach paragraph
+          for (MapEntry<GlobalKey, String> item in searchUtility.entries) {
+            /// if paragraph contains the searchedTerm, store it
+            if (item.value.contains(searchedTerm)) {
+              collectedText.add(item.value);
+            }
+          }
 
-              /// launch popup Dialog
-              dialogBuilder(context, collectedText, searchedTerm);
-            },
-            onEditingComplete: () {}, /// keep keyboard on
-            style: Theme.of(context).textTheme.bodyLarge,
-            cursorColor: Colors.deepPurple,
-            decoration: const InputDecoration(
-                contentPadding: EdgeInsets.all(6.0),
-                labelText: 'Search'),
-          )
-      )
-  );
+          /// launch popup Dialog
+          dialogBuilder(context, collectedText, searchedTerm);
+        },
+        onEditingComplete: () {},
+
+        /// keep keyboard on
+        style: Theme.of(context).textTheme.bodyLarge,
+        cursorColor: Colors.deepPurple,
+        decoration: const InputDecoration(contentPadding: EdgeInsets.all(6.0), labelText: 'Search'),
+      )));
 }
